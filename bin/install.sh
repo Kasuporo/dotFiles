425a923333201705dad80d52ef62f2a79bd168d7
Justin Duch <aphelion030@gmail.com>
2019-01-23 15:05:38 +1100

    install changes

--- bin/install.sh
+++ bin/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FOLDER=$(dirname $0)
+cd $(dirname $0)/../
+FOLDER=`pwd`
 
 backup_file()
 {
     if [[ `ls $HOME/$1 2>/dev/null` ]]; then
         printf "Backing up old $1...\n"
         cp $HOME/$1 $HOME/$1.dotfile.bak
-        rm $HOME/$1
     fi
 }
 
@@ -16,7 +16,7 @@ backup_file()
 if [[ `command -v hyper` ]]; then
     printf "Setting up hyper\n"
     backup_file '.hyper.js'
-    ln $FOLDER/hyper.js $HOME/.hyper.js
+    ln -sFf $FOLDER/hyper.js $HOME/.hyper.js
     printf "Done\n\n"
 fi
 
@@ -24,7 +24,7 @@ fi
 # Setup Vim
 printf "Setting up vimrc\n"
 backup_file '.vimrc'
-ln $FOLDER/vimrc $HOME/.vimrc
+ln -sFf $FOLDER/vimrc $HOME/.vimrc
 mkdir -p $HOME/.vim/backup $HOME/.vim/swap $HOME/.vim/undo
 printf "Installing Vundle\n"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 2>/dev/null
@@ -35,7 +35,7 @@ printf "Done\n\n"
 if [[ `echo $SHELL` = '/bin/zsh' ]]; then
     printf "Setting up zshrc\n"
     backup_file '.zshrc'
-    ln $FOLDER/zshrc $HOME/.zshrc
+    ln -sFf $FOLDER/zshrc $HOME/.zshrc
     printf "Installing Oh My Zsh\n"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     printf "Done\n\n"
@@ -46,7 +46,7 @@ fi
 if [[ `command -v tmux` ]]; then
     printf "Setting up tmux\n"
     backup_file 'tmux.conf'
-    ln $FOLDER/tmux.conf $HOME/tmux.conf
+    ln -sFf $FOLDER/tmux.conf $HOME/tmux.conf
     printf "Done\n\n"
 fi
 
@@ -59,14 +59,14 @@ if [[ ! `command -v ptpython` ]]; then
 fi
 mkdir -p $HOME/.ptpython
 backup_file '.ptpython/config.py'
-ln $FOLDER/ptpython/config.py $HOME/.ptpython/config.py
+ln -sFf $FOLDER/ptpython/config.py $HOME/.ptpython/config.py
 printf "Done\n\n"
 
 
 # Setup Ctags
 printf "Setting up ctags\n"
 backup_file '.ctags'
-ln $FOLDER/ctags $HOME/.ctags
+ln -sFf $FOLDER/ctags $HOME/.ctags
 printf "Done\n\n"
 
 
