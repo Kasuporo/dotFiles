6bbacbadd915903a27d56f9c97c23cf78ad046bb
Justin Duch <justin@justinduch.com>
2019-06-19 16:32:28 +1000

    bw

--- bin/bwunlock.sh
+++ bin/bwunlock.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 echo "Enter password:"
-read -rs p;
+read -r -s -p "> " p;
 
 b="$(echo "$p" | bw unlock | rg 'export' | rg -o 'BW_SESSION.+')"
 
-if [[ $b ]]; then
-    export "$b"
+if [ "$b" ]; then
+    export "${b}?"
 fi
