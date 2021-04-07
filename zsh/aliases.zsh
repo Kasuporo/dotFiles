332f5fe0973aa1db3c9022961cabeea60df176bc
Justin Duch <justin@duch.me>
2021-04-07 23:58:10 +1000

    fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -103,4 +103,4 @@ alias bwg="bw generate -lusn --length 24 | pbcopy"
 # I'm an idiot
 alias rm="rm -i"
 alias mv="mv -i"
-alias contessh="/Users/justin/dev/git.duch.me/justin/contessh/contessh.py"
+alias contessh="/Users/justin/dev/git.waifu.church/justin/contessh/contessh.py"
